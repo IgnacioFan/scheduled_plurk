3bfccaa50862907f579fae4853d431ce446e6e63
Weilong <fan01856472@gmail.com>
2025-02-17 18:16:54 +0800

    add posts table

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, func, Text, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -11,3 +11,14 @@ class User(Base):
     created_at = Column(DateTime, nullable=False, default=func.now())
     updated_at = Column(DateTime, nullable=False, default=func.now(), onupdate=func.now())
 
+    posts = relationship("Post", back_populates="user")
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    post_at = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    user = relationship("User", back_populates="posts")
